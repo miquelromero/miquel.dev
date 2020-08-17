c6deb92ed8ed74a1d5f5cf789ee3b2b68cf0c537
Miquel Romero Sanfeliu <miquelromero@outlook.com>
2020-08-17 20:07:18 +0200

    Delete unused boilerplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,14 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Miquel Romero Sanfeliu',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Frontend Developer',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
@@ -51,11 +51,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
-  ],
+  modules: ['@nuxtjs/pwa'],
   /*
    ** Content module configuration
    ** See https://content.nuxtjs.org/configuration
